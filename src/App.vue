1ddbd4b041a6b237ebde9b1c45f38e7bdab98a2b
Agata Grdal <agata.grdal@buzzfeed.com>
2021-07-05 18:31:04 +0100

    minimum working thing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class="info">
+    <h1>Welcome to Among us tracker</h1>
+  </header>
+
+  <main class="flexbox">
+    <Board id="crew"> </Board>
+    <Board id="likely-innocent"> </Board>
+    <Board id="unknown">
+      <Card id="banana" draggable="true"> </Card>
+      <Card id="gray" draggable="true"> </Card>
+      <Card id="brown" draggable="true"> </Card>
+      <Card id="yellow" draggable="true"> </Card>
+      <Card id="rose" draggable="true"> </Card>
+    </Board>
+    <Board id="sus"> </Board>
+    <Board id="impostor"> </Board>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Board from "./components/Board.vue";
+import Card from "./components/AmongUsCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Board,
+    Card,
+  },
+};
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+  margin: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+
+div {
+  border: 1px solid black;
+}
+
+.info {
+  height: 20%;
+}
+
+.flexbox {
+  display: flex;
+  height: 80%;
+  width: 100%;
+}
+
+.board {
+  border: 1px solid blueviolet;
+  width: 20%;
 }
 </style>
---
+++ src/components/AmongUsCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="card"
+    :id="id"
+    :draggable="draggable"
+    @dragstart="dragStart"
+    @dragover.stop
+  >
+    <img
+      :src="require(`../assets/${id}.png`)"
+      :draggable="false"
+      aria-hidden="true"
+    />
+    <slot />
+    ({{ id }})
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id", "draggable"],
+  methods: {
+    dragStart: (e) => {
+      const target = e.target;
+      e.dataTransfer.setData("card_id", target.id);
+      // setTimeout(() => {
+      //   target.style.display = "none";
+      // }, 0);
+    },
+  },
+};
+</script>
