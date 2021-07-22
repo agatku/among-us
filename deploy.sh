951e8af49d15414d77ef3f9a18d6dbf2d68ba5d4
Agata Grdal <agata.grdal@buzzfeed.com>
2021-07-22 12:48:14 +0100

    cleanup, change favicon

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # deploying to https://<USERNAME>.github.io/<REPO>, to gh-pages branch
-git push -f git@github.com:agatku/among-us.git main:gh-pages
+git push -f git@github.com:agatku/among-us.git master:gh-pages
 # git push origin HEAD:main
 
 cd -
